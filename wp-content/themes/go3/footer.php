faf9bdb64712c82097ca41a434f2b16246fe95f2
Jose Martinez <esjosemartinez@gmail.com>
2015-09-30 16:15:55 +0200

    starting template

---
+++ wp-content/themes/go3/footer.php
@@ -0,0 +1,41 @@
+
+			<footer id="page-footer">
+				<div class="footer-container">
+					<ul>
+						<li>VIVO PLAY</li>
+						<li>Cine</li>
+						<li>Series</li>
+						<li>Deportes</li>
+						<li>Infantil</li>
+						<li>documentales</li>
+						<li>Noticias</li>
+						<li>Adultos</li>
+						<li>Karaoke</li>
+					</ul>
+					<ul>
+						<li>TELEVISIÓN</li>
+						<li>Cine</li>
+						<li>Toda la programación</li>
+					</ul>
+					<ul>
+						<li>MIS CONTENIDOS</li> 
+						<li>Mis alquilados</li>
+						<li>Vistos recientemente</li>
+						<li>Lo quiero ver</li>
+						<li>Siguiendo</li>
+						<li>Grabaciones disponibles</li>
+						<li>Grabaciones programadas</li>
+					</ul>
+					<ul>
+						<li>© Movistar</li> 
+						<li>Ayuda</li>
+						<li>Condiciones legales</li>
+						<li>Política de privacidad</li>
+					</ul>
+				</div>
+
+			</footer>
+
+		</div> <!--! end of #container -->
+	</body>
+</html>
---
+++ wp-content/themes/go3/template-page-home.php
@@ -0,0 +1,144 @@
+			<div id="thematic-areas" role="main">
+
+				<section class="thematic-area cine-area">
+					<div class="thematic-info">
+						<a class="flexbox">
+							<h1 class="thematic-title">Cine</h1>
+							<p class="thematic-slogan">El mejor cine ya a tu alcance</p>
+							<a class="link">Ir a Cine</a>
+						</a>
+					</div>
+					<div class="thematic-content">
+						<div class="cover big">
+							<img src="theme/images/covers/areas/ta_edit_big_birdman.png" alt="Birdman">
+						</div>
+					</div>
+				</section>
+				<section class="thematic-area series-area no-editor">
+					<div class="thematic-info">
+						<div class="flexbox">
+							<h1 class="thematic-title">Series</h1>
+							<p class="thematic-slogan">Disfruta de las últimas temporadas de tus series preferidas</p>
+							<a class="link">Ir a series</a>
+						</div>
+					</div>
+					<div class="thematic-content">
+						<div class="cover big">
+							<img src="theme/images/covers/areas/ta_edit_series.png" alt="Series">
+						</div>
+					</div>
+				</section>
+
+				<section class="thematic-area sport-area">
+					<div class="thematic-info">
+						<div class="flexbox">
+							<h1 class="thematic-title">Deportes</h1>
+							<p class="thematic-slogan">Todos tus deportes favoritos</p>
+							<a class="link">Ir a Deportes</a>
+						</div>
+					</div>
+					<div class="thematic-content">
+						<div class="cover big">
+							<img src="theme/images/covers/areas/ta_edit_sports.png" alt="Deportes">
+						</div>
+					</div>
+				</section>
+
+				<section class="thematic-area kids-area">
+					<div class="thematic-info">
+						<div class="flexbox">
+							<h1 class="thematic-title">Infantil</h1>
+							<p class="thematic-slogan">Horas de entretenimiento para los más peques</p>
+							<a class="link">Ir a Infantil</a>
+						</div>
+					</div>
+					<div class="thematic-content">
+						<div class="cover big">
+							<img src="theme/images/covers/areas/ta_edit_kids.png" alt="Infantil">
+						</div>
+					</div>
+				</section>
+
+				<section class="thematic-area renting">
+					<div class="thematic-info">
+						<div class="flexbox">
+							<img src="theme/images/icon_ticket.png" alt="icon ticket"/>
+							<h1 class="thematic-title">Taquilla</h1>
+							<p class="thematic-slogan">Contenidos especialmente seleccionados para ti</p>
+							<a class="link">Ir a Taquilla</a>
+						</div>
+					</div>
+					<div class="thematic-content">
+						<div class="cover big">
+							<img src="theme/images/covers/areas/ta_edit_renting.png" alt="Taquilla">
+						</div>
+					</div>
+				</section>
+
+				<section class="section-more">
+					<h1 class="title">Mucho más contenido por descubrir</h1>
+					<ul class="flexbox">
+						<li class="box">
+							<h1 class="area-name">Adultos</h1>
+							<a class="link">Ir a Adultos</a>
+						</li>
+						<li class="box">
+							<h1 class="area-name">Documentales</h1>
+							<a class="link">Ir a Documentales</a>
+						</li>
+						<li class="box">
+							<h1 class="area-name">Karaoke</h1>
+							<a class="link">Ir a Karaoke</a>
+						</li>
+						<li class="box">
+							<h1 class="area-name">Noticias</h1>
+							<a class="link">Ir a Noticias</a>
+						</li>
+					</ul>
+				</section>
+
+			</div>
+
+
+			<div class="secondary-content">
+				<section class="section-editors-pick">
+					<div class="section-container">
+						<h1 class="title">Destacados</h1>
+						<ul class="picks">
+							<li>
+								<img src="theme/images/covers/areas/ta_picks.png" alt="Editor's pick">
+							</li>
+						</ul>
+					</div>
+
+				</section>
+
+				<section class="section-tv">          
+					<div class="section-container">
+						<ul class="picks">
+							<li>
+								<img src="theme/images/covers/areas/ta_tv.png" alt="Televisión">
+							</li>
+						</ul>
+					</div>
+				</section>
+
+				<section class="section-mycontent">
+					<div class="section-container">
+						<ul class="picks">
+							<li>
+								<img src="theme/images/covers/areas/ta_mycontent.png" alt="Mis contenidos">
+							</li>
+						</ul>
+					</div>
+				</section>
+
+				<section class="section-promos">
+					<div class="section-container">
+						<img src="theme/images/covers/areas/ta_promos.png" alt="Promociones">
+					</div>
+				</section>
+
+
+
+			</div> <!--secondary content-->
