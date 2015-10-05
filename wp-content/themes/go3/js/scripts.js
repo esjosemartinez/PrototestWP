411eb815e8b372629a08ddff7c0432fe41414f47
Jose Martinez <esjosemartinez@gmail.com>
2015-10-05 11:07:45 +0200

    all downloaded files from ftp

--- wp-content/themes/go3/js/scripts.js
+++ wp-content/themes/go3/js/scripts.js
@@ -6,10 +6,4 @@ $( document ).ready(function() {
 			$('ul#thematicarea-list>li').fadeIn(200);
 		});
 	});
-	$('.filters-button').on('click', function(){
-		$('ul#thematicarea-list>li').fadeOut(300, function() {
-			tinysort('ul#thematicarea-list>li',{attr:'data-duration'});
-			$('ul#thematicarea-list>li').fadeIn(200);
-		});
-	});
 });
