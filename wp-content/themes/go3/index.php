0403db96250504f82d5f967ff298704301464e7f
fmorozzo <fmorozzo@gmail.com>
2015-10-15 17:46:58 +0200

    post-channel-page

--- wp-content/themes/go3/index.php
+++ wp-content/themes/go3/index.php
@@ -10,6 +10,10 @@ elseif(('pt_films' == get_post_type() || 'pt_tvseries' == get_post_type() || 'pt
 {
 	get_template_part( 'template', 'post-thematic-area' );
 }
+elseif('pt_channels' == get_post_type() && is_archive())
+{
+	get_template_part( 'template', 'post-channel-page' );
+}
 
 get_footer();
 
