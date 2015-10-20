6c22dc94bfcdc7116f8277713b0ad34661bbb805
Jose Martinez <esjosemartinez@gmail.com>
2015-10-20 19:34:37 +0200

    solucionado EPG y dropdown de EPG

--- wp-content/themes/go3/template-page-tvguide-program.php
+++ wp-content/themes/go3/template-page-tvguide-program.php
@@ -2,6 +2,7 @@
 
 	<section id="contentOnNow" class="content-section">
 		<img class="cover" src="/theme/images/placeholder_tvguide_live2.png" alt="toda la programación" >
+		<a href="/guia-tv-en-directo" class="epg-full-link"></a>
 	</section>
 
 </main>
