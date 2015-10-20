f3e9c08aac4d1c41aeed989a3bb4f037df628a2d
Jose Martinez <esjosemartinez@gmail.com>
2015-10-20 18:19:07 +0200

    EPG

--- wp-content/themes/go3/template-page-tvguide-program.php
+++ wp-content/themes/go3/template-page-tvguide-program.php
@@ -1,7 +1,7 @@
 <main class="content">
 
 	<section id="contentOnNow" class="content-section">
-		<iframe src=http://enjoy.pdi.tid.es/paytvgo/release/3.0.0/rfa/prod/es/#/tv-guide/epg" width="1024" height="2000" border="0"/>
+		<img class="cover" src="/theme/images/placeholder_tvguide_live2.png" alt="toda la programación" >
 	</section>
 
 </main>
